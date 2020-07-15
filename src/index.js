bbf7171d141e1633f1d9bcfd3c0814139212dc9b
Aguevarab0729 <angelica0729@gmail.com>
2020-07-15 10:59:03 -0500

    Modificacion validator y maskify

--- src/index.js
+++ src/index.js
@@ -1,56 +1,52 @@
 import validator from './validator.js';
 
-// Seleccionar producto para abrir formulario de validacion de pago
-
-let comprarProducto1 = document.getElementById('comprarProductos1');
-comprarProducto1.addEventListener('click', comprar);
-let comprarProducto2 = document.getElementById('comprarProductos2');
-comprarProducto2.addEventListener('click', comprar);
 function comprar() {
-  document.getElementById('vista2').style.display = "block";
-  document.getElementById('vista1').style.display = "none";
+  document.getElementById('vista3').style.display = "block";
+  document.getElementById('vista2').style.display = "none";
 } 
-
+comprar();
 // Formulario en el cual despues de dar en el boton confirmar pago se ejecuta el validator
-let validarP = document.getElementById('botonValidar');
-validarP.addEventListener('click', validarPago)
+/*let validarP = document.getElementById('botonValidar');
+validarP.addEventListener('click', validarPago)*/
+
 function validarPago() {
   document.getElementById('vista1').style.display="none";
   document.getElementById('vista2').style.display="none";
   document.getElementById('vista3').style.display="block";
+  document.getElementById('vista4').style.display="block";
 }
-
+console.log(validarPago());
 //funcion para volver al inicio y cancelar transacción 
-const cancelarPago = document.getElementById('volver');
-cancelarPago.addEventListener('click', cancelar)
+/*const cancelarPago = document.getElementById('volver');
+cancelarPago.addEventListener('click', cancelar)*/
+
 function cancelar() {
-  document.getElementById('volver');
+  document.getElementById('vista1').style.display="block";
+  document.getElementById('vista2').style.display="none";
+  document.getElementById('vista3').style.display="none";
 }
+cancelar();
 
-let numeroTarjeta = document.pago.numeroTarjeta.value;
-let enmascarar = validator.maskify(numeroTarjeta);
-let validar = validator.isValid(numeroTarjeta);
+const creditCardNumber = document.getElementById('creditCardNumber').value;
+const enmascarar = document.getElementById('tarjetaEnmascarada');
+enmascarar = document.write() = validator.maskify(creditCardNumber); //innerHTML o textContent hubiese servido mejor 
 
-if (numeroTarjeta === '' ||  NaN || numeroTarjeta === ' '){
+let validar = validator.isValid(creditCardNumber);
+
+if (creditCardNumber === '' ||  NaN || creditCardNumber === ' '){
   alert('Si deseas continuar con la compra ingrese un numero de tarjeta');
 } 
 else {
-  validar = validator.isValid(numeroTarjeta);
+  validar = validator.isValid(creditCardNumber);
  }
 
  if (validar) {
-  numeroTarjeta = document.pago.numeroTarjeta.value;
-  document.getElementById('').innerHTML = 'Su tarjeta de credito ' + enmascarar +''+ 'es VALIDA';
+  creditCardNumber = document.getElementById('creditCardNumber');
+  document.getElementById('vista4').innerHTML = 'Su tarjeta de credito ' + enmascarar +''+ 'es VALIDA';
  }
 else {
-  numeroTarjeta = document.pago.numeroTarjeta.value;
-  document.getElementById('').innerHTML = 'Su tarjeta de credito' + enmascarar +''+ 'es INVALIDA, por favor verifique que haya ingresado bien el numero de su tarjeta'
+  creditCardNumber = document.getElementById('creditCardNumber');
+  document.getElementById('vista4').innerHTML = 'Su tarjeta de credito' + enmascarar +''+ 'es INVALIDA, por favor verifique que haya ingresado bien el numero de su tarjeta'
 }
 
-console.log(validator);
-
-/*
-  .classList.remove('hide');
-  .classList.add('hide');*/
-
-  
+console.log(validator);  
--- src/validator.js
+++ src/validator.js
@@ -5,17 +5,17 @@ const validator = {  // funcion que valida la tarjeta de credito
       
       let arrayCreditCardNumber = creditCardNumber.split(''); //.split divide el numero completo de la tarjeta de crédito separando cada numero del string en subcadenas para poder llevar a cabo el algoritmo.
       arrayCreditCardNumber = arrayCreditCardNumber.reverse(); //.reverse invierte el orden de los números. El pimer elemento pasa a ser el ultimo y el ultimo al primero.
-      const creditCardNumber = arrayCreditCardNumber.join(''); //.split vuelve a unir los numeros del string y con const definimos creditCarNumber para que este valor no se pueda volver a asignar o declarar, practicamente hace lo contrario a .split.
+      creditCardNumber = arrayCreditCardNumber.join(''); //.split vuelve a unir los numeros del string y con const definimos creditCarNumber para que este valor no se pueda volver a asignar o declarar, practicamente hace lo contrario a .split.
 
       //bucle o ciclo "for" con las condiciones necesiarias para recorrer los numeros uno a uno en orden ascendente. 
       for (let i = 0; i < longitud; i++) {
         let numeros = parseInt(creditCardNumber[i]); //Convertir el string en numeros con parseInt y [i] para hacerlo un array de numeros y poder hacer las operaciones matematicas.
-        if (i % 2 === 0) {  //genera o identifica los valores pares.
+        if (i % 2 === 0) {  //genera o identifica los valores pares por el residuo.
           numeros *= 2;  //Multiplica los números pares * 2
-          if (numeros > 9)  //Identifica de estos numeros pares los que son mayores a 9.
+          if (numeros > 9);  //Identifica de estos numeros pares los que son mayores a 9.
           numeros -= 9;  // Si la multiplicación es mayor a 9, reste 9.
         }
-        sumaNumeros + numeros  //Suma todos los numeros de la tarjeta para ser valida
+        sumaNumeros = sumaNumeros + numeros;  //Suma todos los numeros de la tarjeta para ser valida
       }
       if (sumaNumeros % 10 === 0) //Si la sumatoria de los digitos es modulo de 10, el residuo debe ser 0, es valida.
       return true;
@@ -24,10 +24,9 @@ const validator = {  // funcion que valida la tarjeta de credito
   
     maskify: (creditCardNumber) => {
       const longitud = creditCardNumber.lenght;
-      if (longitud < 4) 
-      return creditCardNumber;
-      return '#'.repeat(longitud - 4) + creditCardNumber.slice(-4); //4 ultimos digitos se mantienen y con .slice hacemos o devolvemos una copia de una parte del array dentro de uno nuevo, ya enmasacaradp
-      
+      if (longitud < 4) {
+      return creditCardNumber;}
+      return '#'.repeat(longitud - 4) + creditCardNumber.slice(-4); //4 ultimos digitos se mantienen y con .slice hacemos o devolvemos una copia de una parte del array dentro de uno nuevo, ya enmasacarado
     }
   };
   
